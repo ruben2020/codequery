f792f276e982ec29007c2110d48671694860f3d4
Ruben <ruben@ruben-laptop.(none)>
2013-02-24 23:50:11 +0800

    updated CLI

--- cli/main_cli.cpp
+++ cli/main_cli.cpp
@@ -29,21 +29,21 @@
 
 void printhelp(const char* str)
 {
-	printf("Usage: %s [-s <sqdbfile> [-p n] [-t x] -[e|f] ] [-d] [-v] [-h]\n\n", str);
+	printf("Usage: %s [-s <sqdbfile> [-p <n>] [-t <term>] -[e|f] ] [-d] [-v] [-h]\n\n", str);
 	printf("options:\n");
-	printf("  -s   : CodeQuery sqlite3 db file path\n");
-	printf("  -p n : parameter is a number denoted by n\n");
-	printf("         default n = 1 (Symbol)\n");
-	printf("  -t x : search term without spaces\n");
-	printf("         if Exact Match is switched off, wild card\n");
-	printf("         searches are possible. Use * and ?\n");
-	printf("  -e   : Exact Match switched ON \n");
-	printf("         Case-sensitive\n");
-	printf("  -f   : Exact Match switched OFF (fuzzy search)\n");
-	printf("         Case-insensitive with wild card search\n");
-	printf("  -d   : debug\n");
-	printf("  -v   : version\n");
-	printf("  -h   : help\n\n");
+	printf("  -s : CodeQuery sqlite3 db file path\n");
+	printf("  -p : parameter is a number denoted by n\n");
+	printf("       default n = 1 (Symbol)\n");
+	printf("  -t : search term without spaces\n");
+	printf("       if Exact Match is switched off, wild card\n");
+	printf("       searches are possible. Use * and ?\n");
+	printf("  -e : Exact Match switched ON \n");
+	printf("       Case-sensitive\n");
+	printf("  -f : Exact Match switched OFF (fuzzy search)\n");
+	printf("       Case-insensitive with wild card search\n");
+	printf("  -d : debug\n");
+	printf("  -v : version\n");
+	printf("  -h : help\n\n");
 	printf("The combinations possible are -s -t -e, -s -t -f\n");
 	printf("The additional optional arguments are -d\n\n");
 	printf("The possible values for n are:\n");
@@ -58,7 +58,7 @@ void printhelp(const char* str)
 	printf("    9: Class which owns this member or method\n");
 	printf("   10: Children of this class (inheritance)\n");
 	printf("   11: Parent of this class (inheritance)\n\n");
-	printf("Example:\n%s -s myproject.db -p 5 -t read*file -f\n\n", str);
+	printf("Example:\n%s -s myproject.db -p 6 -t read*file -f\n\n", str);
 
 }
 
@@ -190,11 +190,13 @@ int main(int argc, char *argv[])
 				bExact = true;
 				countExact++;
 				bError = bError || (countExact > 1);
+				if (countExact > 1) printf("Error: either -e or -f but not both!\n");
 				break;
 			case 'f':
 				bExact = false;
 				countExact++;
 				bError = bError || (countExact > 1);
+				if (countExact > 1) printf("Error: either -e or -f but not both!\n");
 				break;
 			case 's':
 				bSqlite = true;

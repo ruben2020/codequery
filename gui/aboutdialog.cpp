d92028ac1361bb42f9bbbe825c499c249b94d64e
Ruben <ruben2020-git@users.sourceforge.net>
2013-07-14 14:05:36 +0200

    Improved About Dialog

---
+++ gui/aboutdialog.cpp
@@ -0,0 +1,46 @@
+
+/*
+ * CodeQuery
+ * Copyright (C) 2013 ruben2020 https://github.com/ruben2020/
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#include "ui_aboutDialog.h"
+#include "aboutdialog.h"
+#include "swver.h"
+
+
+cqDialogAbout::cqDialogAbout(QWidget *parent)
+:QDialog(parent)
+,dialog_ui(new Ui::aboutDialog)
+ {
+	dialog_ui->setupUi(this);
+	dialog_ui->labelTitle->setText(CODEQUERY_SW_VERSION_WEBSITE);
+	dialog_ui->labelLicense->setText(CODEQUERY_SW_LICENSE_PARA_LINK);
+	connect(dialog_ui->pushButtonCloseAbout, SIGNAL(clicked()),
+		this, SLOT(accept()));
+	resize(sizeHint());
+	layout()->setSizeConstraint(QLayout::SetFixedSize) ;
+	setSizeGripEnabled(false) ;
+}
+
+cqDialogAbout::~cqDialogAbout()
+ {
+	disconnect();
+	delete dialog_ui;
+ }
+
+
---
+++ gui/aboutdialog.h
@@ -0,0 +1,45 @@
+
+/*
+ * CodeQuery
+ * Copyright (C) 2013 ruben2020 https://github.com/ruben2020/
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#ifndef _ABOUTDIALOG_H
+#define _ABOUTDIALOG_H
+
+#include <QtGui>
+
+ namespace Ui {
+     class aboutDialog;
+ }
+
+class cqDialogAbout : public QDialog
+{
+  Q_OBJECT
+
+public:
+Ui::aboutDialog *dialog_ui;
+cqDialogAbout(QWidget *parent);
+virtual ~cqDialogAbout();
+
+
+};
+
+
+#endif //_GRAPHDIALOG_H
+
+

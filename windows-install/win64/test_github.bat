1f58270b6e91921bb752f72fa50195717ad1f5fc
ruben2020 <ruben2020-git@users.sourceforge.net>
2022-04-16 21:14:26 +0800

    Github Actions update for Windows

---
+++ windows-install/win64/test_github.bat
@@ -0,0 +1,6 @@
+build\cqmakedb.exe -v
+"windows-install\win64\cscope.exe" -cbR
+"windows-install\win64\ctags.exe" --fields=+i -nR
+build\cqmakedb.exe -s cq.db -c cscope.out -t tags -p -d
+build\cqsearch.exe -s cq.db -p 1 -t CODEQUERY_SW_VERSION -u
+

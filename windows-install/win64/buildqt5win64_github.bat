a3012a11523d21bef1dc231cf67527eb5cd6f96c
ruben2020 <ruben2020-git@users.sourceforge.net>
2022-04-17 01:02:42 +0800

    Updated build files for windows

--- windows-install/win64/buildqt5win64_github.bat
+++ windows-install/win64/buildqt5win64_github.bat
@@ -2,6 +2,7 @@ set PATH=%Qt5_DIR%/bin;%PATH%
 cmake --version
 gcc -v
 g++ -v
+dir /b/a/s c:\vcpkg\sqlite*.dll
 md build
 cd build
 cmake -G "MinGW Makefiles" -DBUILD_QT5=ON -DGHAWIN=ON -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE="c:\vcpkg\scripts\buildsystems\vcpkg.cmake" ..

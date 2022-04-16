fde961299833d1e35d846ef3595912acfee4059c
ruben2020 <ruben2020-git@users.sourceforge.net>
2022-04-17 02:36:52 +0800

    still fixing wix

--- windows-install/win64/buildqt5win64_github.bat
+++ windows-install/win64/buildqt5win64_github.bat
@@ -2,7 +2,6 @@ set PATH=%Qt5_DIR%/bin;%PATH%
 cmake --version
 gcc -v
 g++ -v
-dir /b/a/s c:\vcpkg\sqlite*.dll
 md build
 cd build
 cmake -G "Ninja" -DBUILD_QT5=ON -DGHAWIN=ON -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE="c:\vcpkg\scripts\buildsystems\vcpkg.cmake" ..
--- windows-install/win64/pack_github.bat
+++ windows-install/win64/pack_github.bat
@@ -2,6 +2,9 @@ set PATH=%PATH%;"%WIX%\bin"
 cd build
 md output
 copy *.exe output
+dir /b/a/s C:\ProgramData\Chocolatey\lib\mingw\tools\libgcc*.dll
+dir /b/a/s C:\ProgramData\Chocolatey\lib\mingw\tools\libstdc*.dll
+dir /b/a/s C:\ProgramData\Chocolatey\lib\mingw\tools\libwinpthread*.dll
 copy "c:\vcpkg\installed\x64-windows\bin\sqlite3.dll" output
 cd output
 windeployqt codequery.exe

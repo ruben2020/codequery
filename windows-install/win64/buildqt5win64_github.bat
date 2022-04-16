7176bf8235777b30384c195054bcad03532db491
ruben2020 <ruben2020-git@users.sourceforge.net>
2022-04-16 20:40:26 +0800

    GHAWIN

--- windows-install/win64/buildqt5win64_github.bat
+++ windows-install/win64/buildqt5win64_github.bat
@@ -6,3 +6,10 @@ md build
 cd build
 cmake -G "MinGW Makefiles" -DBUILD_QT5=ON -DGHAWIN=ON -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE="c:\vcpkg\scripts\buildsystems\vcpkg.cmake" ..
 mingw32-make
+cd ..
+build\cqmakedb.exe -v
+"windows-install\win64\cscope.exe" -cbR
+"windows-install\win64\ctags.exe" --fields=+i -nR
+build\cqmakedb.exe -s cq.db -c cscope.out -t tags -p -d
+build\cqsearch.exe -s cq.db -p 1 -t CODEQUERY_SW_VERSION -u
+

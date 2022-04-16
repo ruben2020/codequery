6aff05f34c9c413d0e378e12ab5c352cd3633dba
Ruben <ruben2020-git@users.sourceforge.net>
2022-04-16 20:06:36 +0800

    Update buildqt5win64_github.bat

--- windows-install/win64/buildqt5win64_github.bat
+++ windows-install/win64/buildqt5win64_github.bat
@@ -2,8 +2,8 @@ set PATH=%Qt5_DIR%/bin;%PATH%
 cmake --version
 gcc -v
 g++ -v
-dir /b/a/s c:\vcpkg.cmake
-dir /b/a/s d:\vcpkg.cmake
+dir /b/a/s c:\vcpkg\vcpkg.cmake
+dir /b/a/s c:\vcpkg\*sqlite*.*
 md build
 cd build
 cmake -G "MinGW Makefiles" -DBUILD_QT5=ON -DGHAWIN=ON ..

9e7ba03240f852e05c4f7f32343984c4b4f3869c
ruben2020 <ruben2020-git@users.sourceforge.net>
2022-04-16 23:17:50 +0800

    Updated windows build

--- windows-install/win64/buildqt5win64.bat
+++ windows-install/win64/buildqt5win64.bat
@@ -1,13 +1,13 @@
 cd c:\workspace\codequery
 md build
 cd build
-cmake -G "MinGW Makefiles" -DBUILD_QT5=ON -DSQLITE_INCLUDE_DIR=c:\qt\Tools\mingw730_64\x86_64-w64-mingw32\include -DSQLITE_LIBRARY_RELEASE=c:\qt\Tools\mingw730_64\bin\sqlite3.dll ..
-mingw32-make
+cmake -G "Ninja" -DBUILD_QT5=ON -DSQLITE_INCLUDE_DIR="%MinGW64_DIR%\x86_64-w64-mingw32\include" -DSQLITE_LIBRARY_RELEASE="%MinGW64_DIR%\bin\sqlite3.dll" ..
+ninja
 md output
 copy *.exe output
-copy c:\Qt\tools\mingw730_64\bin\sqlite3.dll output
+copy "%MinGW64_DIR%\bin\sqlite3.dll" output
 cd output
 windeployqt --release codequery.exe
 dir/b/a/s
-candle.exe -ext WixUIExtension -ext WixUtilExtension c:\workspace\codequery\windows-install\win64\codequery64.wxs
+candle.exe -ext WixUIExtension -ext WixUtilExtension "c:\workspace\codequery\windows-install\win64\codequery64.wxs"
 light.exe -ext WixUIExtension -ext WixUtilExtension codequery64.wixobj

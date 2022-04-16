c689a1776ce8243528db5b1e3db8fca8a6fbfadb
Ruben <ruben2020-git@users.sourceforge.net>
2022-04-16 17:20:57 +0800

    Update buildqt5win64_github.bat

--- windows-install/win64/buildqt5win64_github.bat
+++ windows-install/win64/buildqt5win64_github.bat
@@ -1,5 +1,5 @@
 set PATH=%Qt5_DIR%/bin;%Qt5_DIR%/../../Tools/mingw810_64/bin;%Qt5_DIR%/../../Tools/CMake_64/bin;%PATH%
 md build
 cd build
-cmake -G "MinGW Makefiles" -DBUILD_QT5=ON -DSQLITE_INCLUDE_DIR=%Qt5_DIR%/../../Tools/mingw810_64/opt/include -DSQLITE_LIBRARY_RELEASE=%Qt5_DIR%/../../Tools/mingw810_64/opt/lib/sqlite3.21.0/sqlite3210.dll ..
+cmake -G "MinGW Makefiles" -DBUILD_QT5=ON -DSQLITE_INCLUDE_DIR="%Qt5_DIR%/../../Tools/mingw810_64/opt/include" -DSQLITE_LIBRARY_RELEASE="%Qt5_DIR%/../../Tools/mingw810_64/opt/lib/sqlite3.21.0/sqlite3210.dll" ..
 mingw32-make

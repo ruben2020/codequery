ed28c51db4990f16c1d35c134c027a2eaeab2a24
ruben2020 <ruben2020-git@users.sourceforge.net>
2022-04-17 16:58:26 +0800

    Moved Win GHA to Ninja

--- windows-install/win64/buildqt5win64_github.bat
+++ windows-install/win64/buildqt5win64_github.bat
@@ -4,6 +4,6 @@ gcc -v
 g++ -v
 md build
 cd build
-cmake -G "Ninja" -DBUILD_QT5=ON -DGHAWIN=ON -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE="c:\vcpkg\scripts\buildsystems\vcpkg.cmake" ..
+cmake -G Ninja -DBUILD_QT5=ON -DGHAWIN=ON -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE="c:\vcpkg\scripts\buildsystems\vcpkg.cmake" ..
 ninja
 cd ..

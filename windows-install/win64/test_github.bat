32a9bd28b9f0f16fd600c960e61c1f252be1521e
Ruben <ruben2020-git@users.sourceforge.net>
2022-12-19 13:46:12 +0800

    Update test_github.bat

--- windows-install/win64/test_github.bat
+++ windows-install/win64/test_github.bat
@@ -1,14 +1,9 @@
-cd
-echo PATH=%PATH%
-dir/a build\*.exe
 copy "c:\vcpkg\installed\x64-windows\bin\sqlite3.dll" build
 copy "C:\ProgramData\Chocolatey\lib\mingw\tools\install\mingw64\bin\libgcc_s_seh-1.dll" build
 copy "C:\ProgramData\Chocolatey\lib\mingw\tools\install\mingw64\bin\libstdc++-6.dll" build
 copy "C:\ProgramData\Chocolatey\lib\mingw\tools\install\mingw64\bin\libwinpthread-1.dll" build
+"build\cqmakedb.exe" -v
 "windows-install\win64\cscope.exe" -cbR
 "windows-install\win64\ctags.exe" --fields=+i -nR
-dir/a
-"build\cqsearch.exe" -v
-"build\cqmakedb.exe" -v
 "build\cqmakedb.exe" -s cq.db -c cscope.out -t tags -p -d
 "build\cqsearch.exe" -s cq.db -p 1 -t CODEQUERY_SW_VERSION -u

2cde1cc7ceea0c6237c254064a353b015686f7c0
Ruben <ruben2020-git@users.sourceforge.net>
2022-12-19 13:18:37 +0800

    Update test_github.bat

--- windows-install/win64/test_github.bat
+++ windows-install/win64/test_github.bat
@@ -1,8 +1,10 @@
 cd
 echo PATH=%PATH%
-dir/a build
+dir/a build\*.exe
 "windows-install\win64\cscope.exe" -cbR
 "windows-install\win64\ctags.exe" --fields=+i -nR
+dir/a
+"build\cqsearch.exe" -v
 "build\cqmakedb.exe" -v
 "build\cqmakedb.exe" -s cq.db -c cscope.out -t tags -p -d
 "build\cqsearch.exe" -s cq.db -p 1 -t CODEQUERY_SW_VERSION -u

c993b03ab3b1628092861e4fb8e2942a1232f27a
ruben2020 <ruben2020-git@users.sourceforge.net>
2022-12-19 19:44:52 +0800

    Prep v0.26.0

--- makedb/swver.h
+++ makedb/swver.h
@@ -13,7 +13,7 @@
 #ifndef SWVER_H_CQ
 #define SWVER_H_CQ
 
-#define CODEQUERY_SW_VERSION      "CodeQuery 0.25.0"
+#define CODEQUERY_SW_VERSION      "CodeQuery 0.26.0"
 #define CODEQUERY_SW_VERSION_WEBSITE      "<p>"\
 	CODEQUERY_SW_VERSION \
 	"</p>"\

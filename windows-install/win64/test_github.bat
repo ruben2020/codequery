cb6a784ac636b7cc4fcc20de32e72158a65ecb11
Ruben <ruben2020-git@users.sourceforge.net>
2024-01-12 16:51:17 +0800

    Update test_github.bat

--- windows-install/win64/test_github.bat
+++ windows-install/win64/test_github.bat
@@ -1,10 +1,7 @@
-dir/b/a/s "C:\mingw64\libgcc*.dll"
-dir/b/a/s "C:\mingw64\libstdc++*.dll"
-dir/b/a/s "C:\mingw64\libwinpthread*.dll"
 copy "c:\vcpkg\installed\x64-windows\bin\sqlite3.dll" build
-copy "C:\ProgramData\Chocolatey\lib\mingw\tools\install\mingw64\bin\libgcc_s_seh-1.dll" build
-copy "C:\ProgramData\Chocolatey\lib\mingw\tools\install\mingw64\bin\libstdc++-6.dll" build
-copy "C:\ProgramData\Chocolatey\lib\mingw\tools\install\mingw64\bin\libwinpthread-1.dll" build
+copy "C:\mingw64\bin\libgcc_s_seh-1.dll" build
+copy "C:\mingw64\bin\libstdc++-6.dll" build
+copy "C:\mingw64\bin\libwinpthread-1.dll" build
 "build\cqmakedb.exe" -v
 "windows-install\win64\cscope.exe" -cbR
 "windows-install\win64\ctags.exe" --fields=+i -nR

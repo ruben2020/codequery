5e029de8f967348b3fa388b52ea5a3077c1fa92f
Ruben <ruben2020-git@users.sourceforge.net>
2024-01-12 17:13:06 +0800

    Update test_github.bat

--- windows-install/win64/test_github.bat
+++ windows-install/win64/test_github.bat
@@ -7,3 +7,8 @@ copy "C:\mingw64\bin\libwinpthread-1.dll" build
 "windows-install\win64\ctags.exe" --fields=+i -nR
 "build\cqmakedb.exe" -s cq.db -c cscope.out -t tags -p -d
 "build\cqsearch.exe" -s cq.db -p 1 -t CODEQUERY_SW_VERSION -u
+del cscope.out
+del cq.db
+"windows-install\win64\cscope.exe" -bR
+"build\cqmakedb.exe" -s cq.db -c cscope.out -t tags -p -d
+"build\cqsearch.exe" -s cq.db -p 1 -t CODEQUERY_SW_VERSION -u

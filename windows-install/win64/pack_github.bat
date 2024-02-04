f6995e94a50b81233b6d2506539c84568171264e
Ruben <ruben2020-git@users.sourceforge.net>
2024-02-04 12:52:12 +0800

    Update pack_github.bat

--- windows-install/win64/pack_github.bat
+++ windows-install/win64/pack_github.bat
@@ -7,7 +7,7 @@ copy "C:\mingw64\bin\libgcc_s_seh-1.dll" output
 copy "C:\mingw64\bin\libstdc++-6.dll" output
 copy "C:\mingw64\bin\libwinpthread-1.dll" output
 cd output
-windeployqt codequery.exe
+windeployqt --release --verbose 0 --list relative codequery.exe
 dir /b/a/s
 candle.exe -ext WixUIExtension -ext WixUtilExtension "..\..\windows-install\win64\codequery64.wxs"
 light.exe -ext WixUIExtension -ext WixUtilExtension codequery64.wixobj
